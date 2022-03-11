210c03536b8132c4603fe6854aff8924d38980e5
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-03-11 10:44:16 +0300

    fix cache and zoomable case

--- blocks/fr/header/head.php
+++ blocks/fr/header/head.php
@@ -13,6 +13,9 @@
 	?>
 
 	<head>
+		<meta http-equiv="cache-control" content="no-cache">
+		<meta http-equiv="expires" content="0">
+
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
