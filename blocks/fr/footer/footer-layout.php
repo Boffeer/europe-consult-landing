43d49bf96a9538c0145e4604cad47f32da401e54
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-03-16 01:46:28 +0300

    change input tel name

--- blocks/fr/footer/footer-layout.php
+++ blocks/fr/footer/footer-layout.php
@@ -43,7 +43,7 @@
 			<input class="input input--name" name="user_name" type="text" placeholder="Nom - Prenom" maxlength="50">
 		</label>
 		<label class="input__wrap input__wrap--tel">
-			<input class="input input--phone" name="user-tel" type="tel" placeholder="Numero de téléphone" maxlength="25" minlength="10" required inputmode="numeric">
+			<input class="input input--phone" name="user_tel" type="tel" placeholder="Numero de téléphone" maxlength="25" minlength="10" required inputmode="numeric">
 		</label>
 		<button class="button button--primary pop-callback__submit">Réserver le rendez-vous en ligne</button>
 	</form>
--- blocks/ru/footer/footer-layout.php
+++ blocks/ru/footer/footer-layout.php
@@ -32,7 +32,7 @@
 			<input class="input input--name" name="user_name" type="text" placeholder="Имя" maxlength="50">
 		</label>
 		<label class="input__wrap input__wrap--tel">
-			<input class="input input--phone" name="user-tel" type="tel" placeholder="Телефон" maxlength="25" minlength="10" required inputmode="numeric">
+			<input class="input input--phone" name="user_tel" type="tel" placeholder="Телефон" maxlength="25" minlength="10" required inputmode="numeric">
 		</label>
 		<button class="button button--primary pop-callback__submit">Записаться</button>
 	</form>
